8ca486e22c8995860b6d237a0c62c605c00b578b
James Miller <james.rex.miller.4@gmail.com>
2021-09-04 20:00:50 -0600

    Add IconInput styles

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,54 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+
+  const InputWrapper = styled.div`
+    width: fit-content;
+    outline: none;
+    display: flex;
+    align-items: center;
+    border-bottom: ${props => props.size === "large" ? "2px solid black" : "1px solid black"};
+    padding: 4px 0px;
+
+    &:focus {
+      outline: blue;
+      border: 1px solid blue;
+      border-radius: 5px;
+      transition: none;
+    }
+  `
+
+  const Input = styled.input`
+    width: inherit;
+    outline: none;
+    border: none;
+    color: ${COLORS.gray700};
+    padding-left: 10px;
+    text-align: baseline;
+    border-radius: 5px;
+    font-size: ${props => props.size === "large" ? "1.2rem" : "1rem"};
+    font-weight: bold;
+
+    &::placeholder {
+      font-weight: normal;
+      color: ${COLORS.gray500};
+    }
+
+    &:hover {
+      color: black;
+      transition: .5s ease-in-out;
+    }
+  `
+  
+  return (
+    <InputWrapper type="text" tabIndex={1} aria-label={label} size={size}>
+      <Icon id={icon}/>
+      <VisuallyHidden>
+        <label htmlFor="search">{label}</label>
+      </VisuallyHidden>
+      <Input name='search' type="text" placeholder={placeholder} style={{ width }} size={size}/>
+    </InputWrapper>
+  );
 };
 
 export default IconInput;
